add500974bd606a26fe714473c57ae7a01601600
nblintao <nblintao@126.com>
2014-06-16 21:11:28 +0800

    Add grid

---
+++ Test/grid.h
@@ -0,0 +1,20 @@
+#ifndef GRID_H
+#define GRID_H
+
+#include <QObject>
+#include <QList>
+#include <QColor>
+
+class Grid : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Grid(QObject *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // GRID_H
--- Test/main.cpp
+++ Test/main.cpp
@@ -3,10 +3,15 @@
 #include <QQmlEngine>
 #include <QQmlFileSelector>
 #include <QQuickView>
+#include <QQmlContext>
 int main(int argc, char* argv[])
 {
     QGuiApplication app(argc,argv);
     QQuickView view;
+
+   // Grid grids[100][200];
+    //view.rootContext()->setContextProperty("grids",&grids);
+
     view.setSource(QUrl("qrc:///main.qml"));
     view.setResizeMode(QQuickView::SizeRootObjectToView);
     if (QGuiApplication::platformName() == QLatin1String("qnx") ||
