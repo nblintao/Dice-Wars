7408839df2fdc880f61e4c7a87383a79a74d5aac
yhyhyh <893132051@qq.com>
2014-06-23 15:27:07 +0800

    Add explanation in head

--- Test/land.h
+++ Test/land.h
@@ -27,19 +27,19 @@ private:
 public:
     Land();
     ~Land();
-    bool IsAdjacent(Land *anotherLand);
-    int AdjacentLandsNumber();
-    void Attack(Land *attacked,QString& diceAttacker,QString& diceAttacked);
-    void ChangeOwner(Player *newOwner);
-    void AddGrid(Grid* newGrid);
-    void SetColor(QColor color);
-    void SetDice(int diceSum);
-    int GetDice() const;
+    bool IsAdjacent(Land *anotherLand);  //用于判断两领地是否相邻，决定能否相互攻击
+    int AdjacentLandsNumber();  //获取邻接地的数量
+    void Attack(Land *attacked,QString& diceAttacker,QString& diceAttacked);//执行攻击指令，并处理骰子增减等问题
+    void ChangeOwner(Player *newOwner);  //若攻击成功，需要交换领地的归属
+    void AddGrid(Grid* newGrid);  //在创建领地时被调用
+    void SetColor(QColor color);  //设定领地颜色
+    void SetDice(int diceSum);  //设定骰子数量
+    int GetDice() const;    //用于访问私有变量diceAmount
     void Enter();
     void Exit();
     QColor GetColor() const;
-    bool BelongTo(Player *owner);
-    void AddAdjacent(Land *anotherLand);
+    bool BelongTo(Player *owner);  //判断领地是否属于该玩家
+    void AddAdjacent(Land *anotherLand);  //更新与该领地相邻的区域
 };
 
 inline void Land::Enter(){
